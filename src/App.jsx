51ff99ee6c971d42f2d2e232cd512760cc9604ff
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-24 02:07:11 -0300

    feat: create protected route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Login from "./components/login/Login";
 import { UserStorage } from "./UserContext";
+import User from "./components/User/User";
+import ProtectedRoute from "./components/helper/ProtectedRoute";
 
 const App = () => {
   return (
@@ -15,7 +17,15 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
