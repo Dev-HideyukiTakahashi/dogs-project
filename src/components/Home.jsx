3efbd6f640b69f7f44aeef89e279cdd25ddab117
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-26 04:52:47 -0300

    feat: create component head

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Feed from "./feed/Feed";
-import Loading from "./helper/Loading";
+import Head from "./helper/Head";
 
 const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head
+        title="Fotos"
+        description="Home do site dogs, com o feed de fotos."
+      />
       <Feed />
     </section>
   );
