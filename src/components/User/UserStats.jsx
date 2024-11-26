7d58ad4bce3623d1d68cf7e39f3d0c0f311e89a2
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-26 05:34:01 -0300

    feat: create componenet victory graph

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
+import Loading from "../helper/Loading";
+import Error from "../helper/Error";
+import Head from "../helper/Head";
+import { GET_STATS } from "../../api";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return <div>UserStats</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
