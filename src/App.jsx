891fe76f7a21b9eaad2e13c0643ff200a5845a2d
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-26 03:46:40 -0300

    feat: create error 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import User from "./components/User/User";
 import ProtectedRoute from "./components/helper/ProtectedRoute";
 import Photo from "./components/photo/Photo";
 import UserProfile from "./components/User/UserProfile";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
                 </ProtectedRoute>
               }
             />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -5,6 +5,7 @@ import Feed from "../feed/Feed.jsx";
 import UserPhotoPost from "./UserPhotoPost.jsx";
 import UserStats from "./UserStats.jsx";
 import { UserContext } from "../../UserContext.jsx";
+import NotFound from "../NotFound.jsx";
 
 const User = () => {
   const { data } = React.useContext(UserContext);
@@ -16,6 +17,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
